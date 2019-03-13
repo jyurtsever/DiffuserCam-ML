9c55af581c9e8cb250d7c7e2a6d672b8a2a40011
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-03-12 18:17:15 -0700

    Fixed issue with reshaping image data

--- unetTrain.py
+++ unetTrain.py
@@ -58,7 +58,6 @@ def evaluate(model, loss_fn, test_loader):
             batch_idx*len(X_batch), len(test_loader.dataset), 100.*batch_idx / \
                 len(test_loader), loss.item()))
     out = output.cpu().detach().numpy()
-    out = out.reshape((128, 128, 3, out.shape[0]))
     np.save("test_results.npy", out)
 
 def run_train(model, optimizer, loss_fn, train_loader, num_epochs):
