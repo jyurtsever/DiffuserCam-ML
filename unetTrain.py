72f6deb7355f8ecc52fa762ea2b7cea1e4b3289e
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-03-14 10:37:50 -0700

    Save network etc

--- unetTrain.py
+++ unetTrain.py
@@ -58,7 +58,9 @@ def evaluate(model, loss_fn, test_loader):
             batch_idx*len(X_batch), len(test_loader.dataset), 100.*batch_idx / \
                 len(test_loader), loss.item()))
     out = output.cpu().detach().numpy()
-    save_dict = {'pred': out, 'gt': Y_batch, 'recon': X_batch}
+    gt = Y_batch.cpu().detach().numpy()
+    recon = X_batch.cpu().detatch().numpy()
+    save_dict = {'pred': out, 'gt': gt, 'recon': recon}
     io.savemat('test.mat', save_dict)
 
 def run_train(model, optimizer, loss_fn, train_loader, num_epochs):
