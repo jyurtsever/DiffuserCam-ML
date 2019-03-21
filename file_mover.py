cbcc33b72b0037dee1597bae3e666303cfea6eeb
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-03-20 21:23:57 -0700

    Doing cv.imread and eliminated normalization

---
+++ file_mover.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+def main():
+    for i in range(a, b):
+        os.system("")
+
+if __name__ == '__main__':
+    local_dir = sys.argv[1]
+    remote_dir = sys.argv[2]
+    a = int(sys.argv[3])
+    b = int(sys.argv[4])
