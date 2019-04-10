805212d1ba482420b49e2943129feea59aeddb4b
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-04-09 18:17:58 -0700

    more small bug fixes

--- reconNet.py
+++ reconNet.py
@@ -42,6 +42,7 @@ class DiffuserDataset(Dataset):
 
    def __getitem__(self, idx):
        def initialize_img(path):
+           print(path)
            img = cv2.imread(path, -1).astype(np.float32)/512
            if len(img.shape) > 2 and img.shape[2] == 4:
                img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
