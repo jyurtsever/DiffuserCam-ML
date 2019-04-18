75c1a18bb1cdcbedd7f4708c56468b899f6b744e
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-04-18 13:15:37 -0700

    added resnet

--- reconNet.py
+++ reconNet.py
@@ -343,7 +343,7 @@ def conv3x3(in_planes, out_planes, stride=1):
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False, padding=1)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
 class BasicBlock(nn.Module):
