4e0f7fd4d693728977b3eca15468ae39d46a6356
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-09-24 11:35:15 -0700

    first draft of classifier_trainer

--- classifTrain.py
+++ classifTrain.py
@@ -1,10 +1,105 @@
 from resnet import *
+from classifiers import *
+from torch import nn, optim
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
 import argparse
 
 
-def main(args):
-    pass
 
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def main(args):
+    model = resnet18(num_classes=14)
+    if use_gpu:
+        model = model.cuda()
+    lossfn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
+    train(model, optimizer, lossfn, args.num_epochs)
+    test(model)
+
+def train(model, optimizer, lossfn, num_epochs):
+    losses = []
+    iterations = []
+    epoch = 1
+    trainloader = torchdata.DataLoader(train_set, batch_size=args.batch_size, shuffle=False)
+    j = 0
+    for epoch in range(num_epochs):
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+            if use_gpu:
+                inputs, labels = inputs.cuda(), labels.cuda()
+            # print(inputs.shape, labels.shape)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            # print(outputs)
+            loss = lossfn(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 500 == 499:  # print every 2000 mini-batches
+                j += 500
+                print('[%d, %5d] loss: %.3f' %
+                        (epoch + 1, i + 1, running_loss / 500))
+                losses.append(running_loss / 500)
+                iterations.append(j)
+                running_loss = 0.0
+
+    print('Finished Training')
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, "saved_network.pt")
+    np.save("losses.npy", np.array(losses))
+    np.save("iterations.npy", iterations)
+    return model, losses, iterations
+
+def test(model):
+    correct_test = 0
+    total = 0
+    testloader = torchdata.DataLoader(test_set, batch_size=args.batch_size, shuffle=False)
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            if use_gpu:
+                images, labels = images.cuda(), labels.cuda()
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct_test += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct_test / total))
+    correct_train = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            if use_gpu:
+                images, labels = images.cuda(), labels.cuda()
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct_train += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 50000 train images: %d %%' % (
+            100 * correct_train / total))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,5 +107,13 @@ if __name__ == '__main__':
     parser.add_argument("--image_dir", type=str)
     parser.add_argument("--train_names", type=str)
     parser.add_argument("--test_names", type=str)
+    parser.add_argument("--num_epochs", type=str)
+    parser.add_argument("--batch_size", type=str)
     args = parser.parse_args()
+    use_gpu = torch.cuda.is_available()
+
+
+    train_set = DiffuserDatasetClassif(args.train_names, args.data_dir, args.gt_file)
+    test_set = DiffuserDatasetClassif(args.test_names, args.data_dir, args.gt_file)
+
     main(args)
---
+++ classifiers.py
@@ -0,0 +1,163 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as torchdata
+import pandas as pd
+import numpy as np
+import scipy.io as io
+import imageio as imio
+import os
+import random
+import json
+import scipy.misc as scm
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import cv2
+
+from sklearn.model_selection import train_test_split
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+
+
+
+class DiffuserDatasetClassif(Dataset):
+   """Diffuser dataset."""
+
+   def __init__(self, csv_file, data_dir, label_file, num_data=None, transform=None, use_gpu = False, flipud_gt=False):
+       """
+       Args:
+           csv_file (string): Path to the csv file with annotations.
+           data_dir (string): Directory with all the Diffuser images.
+           label_dir (string): Directory with all the natural images.
+           transform (callable, optional): Optional transform to be applied
+               on a sample.
+       """
+       if num_data:
+           self.csv_contents = pd.read_csv(csv_file, nrows=num_data)
+       else:
+           self.csv_contents = pd.read_csv(csv_file)
+       self.data_dir = data_dir
+       self.label_dir = label_file
+       self.transform = transform
+       self.use_gpu = use_gpu
+       f = open(label_file)
+       self.labels = json.load(f)['gt']
+       f.close()
+
+   def __len__(self):
+       return len(self.csv_contents)
+
+   def __getitem__(self, idx):
+       def initialize_img(path, flip=False):
+           #print(path)
+           if flip:
+               img = np.flipud(cv2.imread(path, -1)).astype(np.float32) / 512
+           else:
+               img = cv2.imread(path, -1).astype(np.float32)/512
+           if len(img.shape) > 2 and img.shape[2] == 4:
+               img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+           return img
+
+       img_name = self.csv_contents.iloc[idx,0]
+
+       image = initialize_img(os.path.join(self.data_dir, img_name))
+       label = self.labels[int(img_name[2:7])][img_name]
+       label = torch.Tensor(label)
+       if self.transform:
+           image = self.transform(image)
+
+       if self.use_gpu:
+           image = image.cuda()
+           label = label.cuda()
+
+       sample = {'image': image, 'label': label}
+
+       return sample
+
+class FLIP(object):
+   """Convert ndarrays in sample to Tensors."""
+
+   def __call__(self, sample):
+       image, label = sample['image'], sample['label']
+       n = random.randint(0,2)
+       if n==0:
+           image_new = image.copy()
+       elif n==1:
+           image_new = np.flipud(image.copy())
+       elif n==2:
+           image_new = np.fliplr(image.copy())
+       return {'image': image_new,
+               'label': label}
+
+class FLIP(object):
+   """Convert ndarrays in sample to Tensors."""
+
+   def __call__(self, sample):
+       image, label = sample['image'], sample['label']
+
+       n = random.randint(0,2)
+
+       if n==0:
+           image_new = image.copy()
+           label_new = label.copy()
+       elif n==1:
+           image_new = np.flipud(image.copy())
+           label_new = np.flipud(label.copy())
+       elif n==2:
+           image_new = np.fliplr(image.copy())
+           label_new = np.fliplr(label.copy())
+       return {'image': image_new,
+               'label': label_new}
+
+class ROT90(object):
+   """Convert ndarrays in sample to Tensors."""
+
+   def __call__(self, sample):
+       image, label = sample['image'], sample['label']
+
+       n = random.randint(0,2)
+
+       if n==0:
+           image_new = image.copy()
+           label_new = label.copy()
+       elif n==1:
+           image_new = np.rot90(image.copy(),axes = [1,0])
+           label_new = np.rot90(label.copy(),axes = [1,0])
+       elif n==2:
+           image_new = np.rot90(image.copy(),axes = [0,1])
+           label_new = np.rot90(label.copy(),axes = [0,1])
+       return {'image': image_new,
+               'label': label_new}
+
+
+class SingleColor(object):
+   """Convert ndarrays in sample to Tensors."""
+
+   def __call__(self, sample):
+       image, label = sample['image'], sample['label']
+
+       n = random.randint(0,2)
+       # swap color axis because
+       # numpy image: H x W x C
+       # torch image: C X H X W
+
+       image_new = np.empty([512, 512, 1])
+       label_new = np.empty([512, 512, 1])
+       image_new[:,:,0] = image[:,:,n]
+       label_new[:,:,0] = label[:,:,n]
+
+       return {'image': image_new,
+               'label': label_new}
+
+class ToTensor(object):
+   """Convert ndarrays in sample to Tensors."""
+
+   def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        label = label.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image.copy()).type(torch.FloatTensor),
+                   'label': torch.from_numpy(label.copy()).type(torch.FloatTensor)}
