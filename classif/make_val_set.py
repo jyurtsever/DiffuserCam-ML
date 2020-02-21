36fe5a7d73db9006fb019b59c844d6caa27cf9a3
Joshua Yurtsever <joshua.yurtsver@berkeley.edu>
2020-02-21 00:31:15 -0800

    added train_imagenet.py

--- classif/make_val_set.py
+++ classif/make_val_set.py
@@ -5,28 +5,29 @@ import shutil
 
 
 def main(args):
-    train_dir = args.root + 'train'
-    val_dir = args.root + 'val'
+    train_dir = os.path.join(args.root, 'train')
+    val_dir = os.path.join(args.root, 'val')
+
     if not os.path.isdir(train_dir):
         os.mkdir(train_dir)
 
     if not os.path.isdir(val_dir):
         os.mkdir(val_dir)
-
-    for path, subdirs, files in os.walk(args.root):
+    walk = list(os.walk(args.root))
+    for path, subdirs, files in walk:
         sub_folder = os.path.basename(path)
 
         print(sub_folder)
-        if not sub_folder:
+        if not sub_folder or sub_folder == 'train' or sub_folder == 'val':
             continue
 
-        if not os.path.isdir(val_dir + sub_folder):
-            os.mkdir(val_dir + sub_folder)
+        if not os.path.isdir(os.path.join(val_dir, sub_folder)):
+            os.mkdir(os.path.join(val_dir,  sub_folder))
 
         random.shuffle(files)
         val_files = files[:int(len(files) * args.frac_validation)]
         for val_file in val_files:
-            shutil.move(path + '/' + val_file, val_dir + '/' + val_file)
+            shutil.move(os.path.join(path, val_file), os.path.join(val_dir, sub_folder, val_file))
 
         shutil.move(path, train_dir)
 
@@ -34,7 +35,7 @@ def main(args):
 if __name__ == '__main__':
     random.seed(123)
     parser = argparse.ArgumentParser(description='runs forward model on the first n images in a folder')
-    parser.add_argument('root', type=str)
+    parser.add_argument('-root', type=str)
     parser.add_argument('-frac_validation', type=float, default=.08)
     args = parser.parse_args()
 
