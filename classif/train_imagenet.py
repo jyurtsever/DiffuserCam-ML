51e2e3a5cea228c5398278dda8ef3674c6a114d1
Joshua Yurtsever <joshua.yurtsver@berkeley.edu>
2020-05-09 13:31:45 -0700

    added flipping and appropriate transformations for different imagenet training scenarios

--- classif/train_imagenet.py
+++ classif/train_imagenet.py
@@ -284,7 +284,7 @@ def main_worker(gpu, ngpus_per_node, args):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    if args.use_le_admm:
+    if args.use_le_admm or args.flip_diffuser_im:
         if args.flip_diffuser_im:
             print("flipping diffuser images")
             trans = transforms.Compose([FlipUDTrans(), transforms.ToTensor()])
@@ -304,9 +304,10 @@ def main_worker(gpu, ngpus_per_node, args):
                         normalize,
                     ])
         else:
+            print("resizing and normalizin training data")
             trans = transforms.Compose([
-                        transforms.RandomResizedCrop(224),
-                        transforms.RandomHorizontalFlip(),
+                        transforms.Resize(224), #used to be random resized crop
+                        # transforms.RandomHorizontalFlip(),
                         transforms.ToTensor(),
                         normalize,
                     ])
@@ -332,8 +333,8 @@ def main_worker(gpu, ngpus_per_node, args):
     else:
         val_loader = torch.utils.data.DataLoader(
             datasets.ImageFolder(valdir, transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
+                transforms.Resize(224),
+             #   transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 normalize,
             ])),
